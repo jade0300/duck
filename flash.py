14f0b037fec541e5a1346070d19088b7c25239a9
amy_chang <a612251@gmail.com>
2023-11-26 12:49:09 +0800

    no message

--- flash.py
+++ flash.py
@@ -36,4 +36,4 @@ def upload_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=80)
+    app.run(host='0.0.0.0', port=8352, debug = True)
